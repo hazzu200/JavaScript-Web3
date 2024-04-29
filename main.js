eaf40b87e4910dd6417b1c73a1bf1bff8416a63f
ChaitanyaHazarika <chaitanyahazarika9@gmail.com>
2024-04-29 19:47:20 +0530

    added new user class to create a new user with unique 32 bit id

--- main.js
+++ main.js
@@ -3,15 +3,14 @@ const readline = require('readline');
 const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'))
 const { stdin: input, stdout: output } = require('node:process');
 const crypto = require('crypto');
+const fs = require('fs');
 
 const rl = readline.createInterface({ input, output });
 var password = "miku";
 var userAccount = '';
 var userBalance;
 var password;
-var userID;
 var noOfAccountsToBeCreated;
-var userName;
 
 console.log("[+] Etherium Miku Wallet");
 
@@ -31,35 +30,45 @@ class FirstPage {
     showCreateWallet() {
         rl.question("    NOTE: All your accounts will be removed in the current wallet [Y][N]: ", (confirmation) => {
             if (confirmation == "Y" || confirmation == "y") {
-                this.createNewUserGUI();
+                newUser.createNewUserGUI();
             } else if (confirmation == "n" || confirmation == "N") {
-                this.showFirstPage();
+                this.showFirstPage
             }
 
         })
     }
+
+}
+class NewUser{
     createNewUserGUI() {
-        rl.question("\n    Full Name: ", (userName) => {
-            if (!userName) {
-                console.log("    Please type a name");
-                this.createNewUserGUI();
-            } else {
-                rl.question("    No.of accounts should be created in wallet (can be 0): ", (noOfAccounts) => {
-                    if (!noOfAccounts) {
-                        console.log("    Please enter the number of accounts to be created");
-                    } else if (noOfAccounts) {
-                        this.createUser(userName, noOfAccounts);
-                    }
-                })
-            }})
+        rl.question("    No.of accounts should be created in wallet (can be 0): ", (noOfAccounts) => {
+            if (!noOfAccounts) {
+                console.log("    Please enter the number of accounts to be created");
+            } else if (noOfAccounts) {
+                this.createUser(noOfAccounts);
+            }
+        })
     }
-    createUser(userName, noOfAccounts) {
-        userID = crypto.createHash('sha256').update(userName).digest('hex');
+    createUser(noOfAccounts) {
+        crypto.randomBytes(32, (err, buffer)=>{
+            if (err){
+                console.log(err);
+                return;
+            }
+
+        const userID = buffer.toString('hex');
         const walletDetails = web3.eth.accounts.wallet.create(noOfAccounts);
-        
-
-
+        fs.writeFile('userID.txt', userID , (error) => {
+            if (error) {
+                console.log(error);
+            }
+        })
+        return;
+        })
     }
 }
+
+
 var firstPageShow = new FirstPage();
+var newUser = new NewUser();
 firstPageShow.showFirstPage(); 
